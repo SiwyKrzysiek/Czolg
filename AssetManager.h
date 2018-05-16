8a4f86b37f0f9227c0557562b369c160a3ea9f6d
Krzysztof Dąbrowski <k.fajny@gmail.com>
2018-05-16 22:47:16 +0200

    Początek klasy AssetManager

---
+++ AssetManager.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <map>
+#include <string>
+#include <SFML/Graphics.hpp>
+
+/**
+ * \brief Klasa do zarzadania zasobami takimi jak tekstury i czcionki. 
+ */
+//ToDo Wzorzec projektowy Singleton
+class AssetManager //ToDo Wykorzystac Map
+{
+private:
+	std::map<std::string, sf::Font> fonts;
+	std::map<std::string, sf::Texture> textures;
+
+public:
+	AssetManager();
+	~AssetManager();
+
+	void loadFont(std::string name, std::string fileName);
+	sf::Font& getFont(std::string name);
+
+	void loadTexture(std::string name, std::string fileName);
+	sf::Texture& getTexture(std::string name);
+};
+
